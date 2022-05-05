381b31490b0cd3de0e9410f2d9c6667f9ef3ab98
harrdy25 <kukadiya.hardik1@gmail.com>
2022-05-05 11:20:05 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screen/Home';
+import Settings from './src/screen/Settings';
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({})
